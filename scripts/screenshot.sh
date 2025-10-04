886df0a556981706d76bb198f4f561d8aeed7dbf
Pavel Reshetnikov <pavelresh@yandex.ru>
2025-10-04 09:54:58 +0500

    feat: add screenshot to rose-pine

--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -2,10 +2,16 @@
 
 set -o pipefail
 
+config_dir="${XDG_CONFIG_HOME:-$HOME/.config}/dotfiles"
+config_path="$config_dir/screenshot.conf"
 screenshots_dir="${XDG_PICTURES_DIR:-$HOME/Pictures}/screenshots"
 screenshot_path="$screenshots_dir/$(date +'%Y-%m-%d_%H:%M:%S.%3N').png" 
 
-mkdir -p "$screenshots_dir"
+mkdir -p "$screenshots_dir" "$config_dir"
+
+test -f "$config_path" && source "$config_path"
+SLURP_BACKGROUND="${SLURP_BACKGROUND:-FFFFFF50}"
+SLURP_BORDER="${SLURP_BORDER:-000000FF}"
 
 pids="$(pidof -x $(basename $0))"
 running_scripts_number=$(echo "$pids" | wc -w)
@@ -22,5 +28,5 @@ sleep 0.1s
 hyprpicker_pid=$!
 
 trap '[[ -s "$screenshot_path" ]] || rm -f "$screenshot_path"' EXIT
-grim -g "$(slurp)"  - | tee "$screenshot_path" | wl-copy
+grim -g "$(slurp -b $SLURP_BACKGROUND -c $SLURP_BORDER)"  - | tee "$screenshot_path" | wl-copy
 kill $hyprpicker_pid
